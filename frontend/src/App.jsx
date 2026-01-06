3c803f2ff8a5548f85554e77dd56976ef2edd4da
manjumadalabhanvi <mmadalabhanvi@gmail.com>
2026-01-06 21:45:13 +0530

    Front end is connected with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { useState,useEffect } from "react";
+import axios from "axios"
 
 
 function App() {
-
-
+const[message,setMessage]=useState("");
+useEffect(()=>{
+axios.get("http://localhost:5000/")
+.then((res)=>setMessage(res.data))
+.catch((err)=>console.log(err))
+},[])
   return (
     <>
+  <p>{message}</p>
       <h1>Youtube Clone</h1>
     </>
   )
