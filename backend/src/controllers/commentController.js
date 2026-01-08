f62a8644d86c04e95a2c9e09006abbb895bd2119
manjumadalabhanvi <mmadalabhanvi@gmail.com>
2026-01-08 16:56:28 +0530

    feat: get comments by video and by id

--- backend/src/controllers/commentController.js
+++ backend/src/controllers/commentController.js
@@ -15,3 +15,33 @@ export const addComment = async (req, res) => {
     res.status(500).json({ message: "Failed to add comment" });
   }
 };
+
+
+export const getCommentById = async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.id)
+      .populate("userId", "username img");
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching comment" });
+  }
+};
+
+export const getCommentsByVideo = async (req, res) => {
+  try {
+    const comments = await Comment.find({
+      videoId: req.params.videoId,
+    })
+      .populate("userId", "username img")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get comments" });
+  }
+};
--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { addComment } from "../controllers/commentController.js";
+import { addComment,getCommentById,getCommentsByVideo} from "../controllers/commentController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // ADD COMMENT
 router.post("/", authMiddleware, addComment);
+router.get("/single/:id", getCommentById);
+router.get("/:videoId", getCommentsByVideo);
 
 export default router;
